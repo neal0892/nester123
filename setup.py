a2c117af0ac8c2519d1d2fa70acefbd94071c985
Neeraj Pandey <neal0892@gmail.com>
2016-04-24 17:06:47 +0530

    Added files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup   (
+        name           ='nester123',
+        version         = '1.0.0',
+        py_modules    =['nester123'],
+        author         = 'neeraj',
+	url            = 'https://github.com/neal0892/nester123',
+        author_email = 'neal0892@gmail.com',
+        description     = 'A simple printer of nested lines',
+
+        )
